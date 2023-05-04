37fa83c6bbeeeb7171029b3bb3c752c45774478f
d0k9 <vdokukin@bigmir.net>
2023-05-04 12:59:10 +0300

    init: resolve first task

---
+++ 01-read-file/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
+
+
