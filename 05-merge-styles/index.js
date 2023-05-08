5cef9a84fc7cbe2c3b6216fdec473d0de9eefc70
d0k9 <vdokukin@bigmir.net>
2023-05-08 22:50:18 +0300

    feat: resolve fifth task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const stylesDir = './styles';
-const distDir = './project-dist';
+const stylesDir = './05-merge-styles/styles';
+const distDir = './05-merge-styles/project-dist';
 const outputFile = 'bundle.css';
 
 if (!fs.existsSync(distDir)) {
